6c03eee65d9f478800126516e0572a6eaa60d4d9
Juan David Mercado Lopez <se.juanmercado@gmail.com>
2026-01-12 23:55:46 -0500

    Documentar errores en swagger

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using UserRegistration.Api.Application.DTOs;
 using UserRegistration.Api.Application.Interfaces;
+using UserRegistration.Api.Common;
 
 namespace UserRegistration.Api.Controllers
 {
@@ -17,6 +19,9 @@ namespace UserRegistration.Api.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ValidationProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
         public async Task<IActionResult> Create([FromBody] CreateUserRequestDto request)
         {
             await _userService.CreateUserAsync(request);
